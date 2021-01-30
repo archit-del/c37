b4e70faa014463c6d004accff33877528df207e7
archit-del <72654696+archit-del@users.noreply.github.com>
2021-01-30 16:50:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+var database;
+var playerCount, form,game,player;
+var gameState = 0;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+    game = new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+   if(playerCount===4) {
+      game.update (1)
+   }
+   if(gameState===1) {
+       clear();
+       game.play()
+   }
+}
+
